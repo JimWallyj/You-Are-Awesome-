c87c43fac086be7d8c9d01e3ce31ad9fb77b46cd
Jim Walejko <jwalejko@me.com>
2025-09-30 20:57:34 -0700

    Section 2.11 Randomization from Prof. Gallaugher's course for Swift coding.

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,8 +11,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
+    //var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,20 +27,28 @@ class ViewController: UIViewController {
         let messages = ["You Are Awesome!",
                         "You Are Great!",
                         "You Are Fantastic!",
-                        "Fabulous? That's You!"]
+                        "When the Genius Bar needs help, they call you!",
+                        "Fabulous? That's You!",
+                        "You've Got the Design Skills of Jony Ive"]
         
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        var imageNumber = Int.random(in: 0...totalNumberOfImages)
+        let imageName = "image\(imageNumber)"
+        //let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        
+        //messageLabel.text = messages[messageNumber]
+        /*messageNumber += 1
         if messageNumber == messages.count{
             messageNumber = 0
-        }
+        }*/
         // String interpletation.  Any value will get convertyed to a String -->
-        let imageName = "image\(imageNumber)"
+        /*let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
         imageNumber = imageNumber + 1
         if imageNumber == 10 {
             imageNumber = 0
-        }
+        }*/
         
         /*{
                     messageLabel.text = messages[1]
